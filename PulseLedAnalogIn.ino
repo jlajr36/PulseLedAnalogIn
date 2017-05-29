fbf9f03c187f1aeb0420e5610d54a1fdbaeae925
John Amos <jlajr36@gmail.com>
2017-05-28 21:06:38 -0400

    Add files via upload

---
+++ PulseLedAnalogIn.ino
@@ -0,0 +1,15 @@
+int ledPin = 3;
+int analogPin = A0;
+int period = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  period = (1023 - analogRead(analogPin))/2+25;
+  digitalWrite(ledPin, HIGH);
+  delay(period);
+  digitalWrite(ledPin, LOW);
+  delay(period);
+}
